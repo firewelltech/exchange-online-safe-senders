9e23a808193fb753a222e6ad7a147359afac8790
Jeremiah Henry <jhenry@firewelltechnologysolutions.com>
2023-05-30 15:12:32 -0700

    feat: Update all partner tenants
    The script now iterates through every partner tenant and updates
    the "Firewell Technology Solutions Safe Senders" list with values
    from the Safe Domains CSV file.

--- Exchange Online Safe Senders.ps1
+++ Exchange Online Safe Senders.ps1
@@ -1,5 +1,7 @@
 ﻿# Set the log file path
 $logFilePath = "$PSScriptRoot\log.txt"
+# Set parameters for Connect-ExchangeOnline cmdlet for efficiency (passed to the -CommandName parameter)
+$eoCommands = "Connect-ExchangeOnline, Get-TransportRule, Set-TransportRule, New-TransportRule, Disconnect-ExchangeOnline"
 
 # Check for necessary modules
 # AzureAD
@@ -15,6 +17,19 @@ if (-not (Get-Module -ListAvailable -Name AzureAD)) {
     }
     Write-Log -Message "The AzureAD module has been installed"
 }
+# PartnerCenter
+if (-not (Get-Module -ListAvailable -Name PartnerCenter)) {
+    Write-Log -Message "The PartnerCenter module is not installed."
+    try {
+    Install-Module -Name PartnerCenter -force
+    }
+    catch {
+        Write-Log -Message "Failed to install the PartnerCenter module. Please install it manually and re-run the script."
+        Write-Host "Failed to install the PartnerCenter module. Please install it manually and re-run the script."
+        return
+    }
+    Write-Log -Message "The PartnerCenter module has been installed"
+}
 # ExchangeOnlineManagement
 if (-not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
    try {
@@ -27,6 +42,21 @@ if (-not (Get-Module -ListAvailable -Name ExchangeOnlineManagement)) {
    }
 }
 
+# Microsoft.Identity.Client
+if (-not (Get-Module -ListAvailable -Name Microsoft.Identity.Client)) {
+    Write-Log -Message "The Microsoft.Identity.Client module is not installed."
+    try {
+    Install-Module -Name Microsoft.Identity.Client -force
+    }
+    catch {
+        Write-Log -Message "Failed to install the Microsoft.Identity.Client module. Please install it manually and re-run the script."
+        Write-Host "Failed to install the Microsoft.Identity.Client module. Please install it manually and re-run the script."
+        return
+    }
+    Write-Log -Message "The Microsoft.Identity.Client module has been installed"
+}
+
+
 # Function to write log messages to the log file
 function Write-Log {
     param (
@@ -35,77 +65,28 @@ function Write-Log {
 
     $timestamp = Get-Date -format "yyyy-MM-dd HH:mm:ss"
     $logMessage = "$timestamp - $Message"
-    $logMessage | Out-File -FilePath $logFilePath -Append
+    $logMessage | Out-File -FilePath $logFilePath -Append -Encoding utf8
 }
 
-# Prompt the user to enter the Microsoft Tenant Name
-$maxRetryCount = 3
-$retryCount = 0
-do {
-    $strOrganization = Read-Host "Enter the Microsoft tenant name (without the '.onmicrosoft.com' suffix)"
-    # Append the domain suffix to the Microsoft Tenant Name
-    $strTenant = "$($strOrganization).onmicrosoft.com"
-    # Input validation: Check if the input is empty or contains invalid characters
-    if ([string]::IsNullOrEmpty($strOrganization) -or $strOrganization -notmatch "^[a-zA-Z0-9-]+$") {
-        Write-Host "Invalid input. Microsoft Tenant Name cannot be empty or contain special characters."
-    }
-    else {
-        # Check if the tenant name is valid
-        try {
-            $ipAddresses = Resolve-DnsName -Name $strTenant -ErrorAction Stop
-            if ($ipAddresses) {
-                break
-            }
-            else {
-                Write-Log -Message "Invalid Microsoft tenant name. Please check the tenant name and try again."
-                Write-Host "Invalid Microsoft tenant name. Please check the tenant name and try again."
-                $strOrganization = $null
-            }
-        }
-        catch {
-            Write-Log -Message "An error occurred while attempting to connect to '$strTenant'. Please try again."
-            Write-Host "An error occurred while attempting to connect to '$strTenant'. Please try again."
-            $strOrganization = $null
-        }
-    }
-    $retryCount++
-} while ($retryCount -lt $maxRetryCount)
+# Get user credentials
+$credentials = Get-Credential
+$userName = $credentials.UserName
 
-if ($retryCount -ge $maxRetryCount) {
-    Write-Log -Message "Failed to provide a valid Microsoft tenant name after $maxRetryCount attempts. Exiting the script."
-    Write-Host "Failed to provide a valid Microsoft tenant name after $maxRetryCount attempts. Exiting the script."
+function Get-PartnerTenants {
+    # Authenticate with Partner Center credentials
+    Connect-PartnerCenter
+    # Retrieve the customer tenants associated with the partner account and pipe the domain names
+    $customerTenants = Get-PartnerCustomer | Select-Object -ExpandProperty Domain
+
+    # Disconnect
+    Disconnect-PartnerCenter
+    # Return the tenant names
+    return $customerTenants
 }
 
-# Display information
-Write-Host "Authenticating to $strTenant..."
-
-# Authenticate to Exchange Online with a timeout of 30 seconds
-try {
-    $connectionTimeout = New-TimeSpan -Seconds 30
-    $connectionTimer = [Diagnostics.Stopwatch]::StartNew()
-    do {
-        try {
-            Connect-ExchangeOnline -DelegatedOrganization $strTenant
-            break
-        }
-        catch {
-            # Handle authentication errors
-            if ($connectionTimer.Elapsed -gt $connectionTimeout) {
-                Write-Log -Message "Failed to authenticate to Exchange Online. Please check your credentials and the tenant name."
-                Write-Host "Failed to authenticate to Exchange Online. Please check your credentials and the tenant name."
-                return
-            }
-        }
-        Start-Sleep -Milliseconds 500
-    } while ($true)
-    
-}
-finally {
-    $connectionTimer.Stop()
-}
-
-# Set the transport rule name
-$ruleName = "Firewell Technology Solutions Safe Senders"
+# Call the Get-PartnerTenants function to get the domain names of all partner tenants
+# Position 0 in the array (the partner tenant itself) should be omitted from the foreach loop later
+$tenantDomains = Get-PartnerTenants | Where-Object { $_ -like "*.onmicrosoft.com"}
 
 # Read the CSV file with the domain safe list
 $csvFilePath = Join-Path $PSScriptRoot "Safe Domains.csv"
@@ -115,75 +96,88 @@ if (-not (Test-Path $csvFilePath)) {
     return
 }
 
-# Retrieve the existing transport rule, if it exists
-$existingRule = Get-TransportRule -Identity $ruleName -ErrorAction SilentlyContinue
+# Set the transport rule name
+$ruleName = "Firewell Technology Solutions Safe Senders"
 
-if ($existingRule) {
-    # Logging: Existing rule found
-    Write-Log -Message "The transport rule '$ruleName' already exists. Appending domain names if not already present."
+# Set the domain list bound for the -SenderDomainIs parameter.
+$newSafeDomains = Import-Csv -Path $csvFilePath | Select-Object -ExpandProperty Domain
 
-    # Retrieve the existing domain names from the rule
-    $existingDomains = $existingRule.SenderDomainIs
-
-    $newDomains = Import-Csv -Path $csvFilePath | Select-Object -ExpandProperty Domain
-    
-    # Filter out the domain names that are already present in the rule
-    $uniqueDomains = $newDomains | Where-Object { $existingDomains -notcontains $_ }
-
-    if ($uniqueDomains) {
-        # Logging: Domain names to be added
-        Write-Log -Message "The following domain names will be added to the transport rule at '$strTenant':"
-        Write-Log -Message $($uniqueDomains -join ', ')
-        Write-Host "The following domain names will be added to the transport rule at '$strTenant':"
-        Write-Host $($uniqueDomains -join ', ')
-
-        # Add the new domain names to the existing rule
-        $existingRule.Conditions.SenderDomainIs += $uniqueDomains
-
-        # Update the rule
-        Set-TransportRule -Identity $existingRule.Identity -Conditions $existingRule.Conditions
-
-        # Logging: Rule updated
-        Write-Log -Message "The transport rule has been updated with the new domain names."
-        Write-Host "The transport rule has been updated with the new domain names."
+foreach ($tenantDomain in $tenantDomains) {
+    # Establish ExchangeOnline connection
+    try {
+        Connect-ExchangeOnline -DelegatedOrganization $tenantDomain -ShowProgress $true -UserPrincipalName $userName
     }
-    else {
-        # Logging: No domain names to add
-        Write-Log -Message "No new domain names to add to the transport rule at '$strTenant'."
-        Write-Host "No new domain names to add to the transport rule at '$strTenant'."
+    catch {
+        Write-Log -Message "Could not connect to $($tenantDomain)."
+        Write-Host "Could not connect to $($tenantDomain)."
+        return
     }
-}
+    # Retrieve the existing transport rule, if it exists
+    $existingRule = Get-TransportRule -Identity $ruleName -ErrorAction SilentlyContinue
 
-elseif ($null = $existingrule) {
-# Read safe domains from CSV file
-$safeDomains = Import-Csv -Path $csvFilePath | Select-Object -ExpandProperty Domain
+    # If the rule already exists...
+    if ($existingRule) {
+        # Logging: Existing rule found
+        Write-Log -Message "The transport rule '$($ruleName)' already exists. Overwriting with the current safe sender list if it's different."
 
-if (-not $safeDomains) {
-    Write-Log -Message "No safe domains found in the CSV file."
-    Write-Host "No safe domains found in the CSV file."
-    return
-}
+        # Retrieve the existing domain names from the rule
+        $existingSafeDomains = $existingRule.SenderDomainIs
 
-# Create the transport rule for safe senders
-Write-Output "Creating transport rule '$ruleName'..."
-New-TransportRule -Name $ruleName `
-    -Comments "Authenticated emails from these domains should never be filtered or blocked" `
-    -Enabled $true `
-    -HasNoClassification $false `
-    -HeaderContainsMessageHeader Authentication-Results `
-    -HeaderContainsWords "dmarc=pass", "dmarc=bestguesspass" `
-    -Mode Enforce `
-    -RecipientAddressType Resolved `
-    -RuleErrorAction Ignore `
-    -RuleSubType None `
-    -SenderAddressLocation Header `
-    -SenderDomainIs $safeDomains `
-    -SetHeaderName X-ETR `
-    -SetHeaderValue "Bypass spam filtering for authenticated sender" `
-    -SetSCL -1
+        # Filter out the domain names that are already present in the rule
+        $uniqueSafeDomains = $newSafeDomains | Where-Object { $existingSafeDomains -notcontains $_ }
 
-Write-Log -Message "The following domains were added to the Firewell Technology Solutions 'Safe Senders' list at '$strTenant' under the rule named '$ruleName':"
-Write-Log -Message $($safeDomains -join ', ')
-Write-Output "The following domains were added to the Firewell Technology Solutions 'Safe Senders' list at '$strTenant':"
-Write-Output $($safeDomains -join ', ')
+        # If there are names that weren't already in .SenderDomainIs...
+        if ($uniqueSafeDomains) {
+            # Logging: Domain names to be added
+            Write-Log -Message "The rules from the source file at $($csvFilePath) are different, so we will invoke Set-TransportRule for '$tenantDomain' and add the following domains to the safe-list:"
+            Write-Log -Message $($newSafeDomains -join ', ')
+            Write-Host "The rules from the source file at $($csvFilePath) are different, so we will invoke Set-TransportRule for '$tenantDomain' and add the following domains to the safe list:"
+            Write-Host $($newSafeDomains -join ', ')
+
+            # Set the new rule
+            Set-TransportRule -Identity $existingRule.Identity -SenderDomainIs $newSafeDomains
+
+            # Logging: Rule updated
+            Write-Log -Message "The transport rule has been updated with the new domain names."
+            Write-Host "The transport rule has been updated with the new domain names."
+        }
+        else {
+            # Logging: No new domain names to add
+            Write-Log -Message "No new domain names to add to the transport rule at '$tenantDomain'."
+            Write-Host "No new domain names to add to the transport rule at '$tenantDomain'."
+        }
+    }
+
+    # If the rule doesn't already exist, let's create it!
+    elseif ($null = $existingrule) {
+        # Make sure the CSV file isn't blank or cannot be found
+        if (-not $newSafeDomains) {
+            Write-Log -Message "No safe domains found in the CSV file."
+            Write-Host "No safe domains found in the CSV file."
+            return
+        }
+
+        # Create the transport rule for safe senders
+        Write-Output "Creating transport rule '$ruleName'..."
+        New-TransportRule -Name $ruleName `
+            -Comments "Authenticated emails from these domains should never be filtered or blocked" `
+            -Enabled $true `
+            -HasNoClassification $false `
+            -HeaderContainsMessageHeader Authentication-Results `
+            -HeaderContainsWords "dmarc=pass", "dmarc=bestguesspass" `
+            -Mode Enforce `
+            -RecipientAddressType Resolved `
+            -RuleErrorAction Ignore `
+            -RuleSubType None `
+            -SenderAddressLocation Header `
+            -SenderDomainIs $newSafeDomains `
+            -SetHeaderName X-ETR `
+            -SetHeaderValue "Bypass spam filtering for authenticated sender" `
+            -SetSCL -1
+
+        Write-Log -Message "The following domains were added to the Firewell Technology Solutions 'Safe Senders' list at '$tenantDomain' under the rule named '$ruleName':"
+        Write-Log -Message $($newSafeDomains -join ', ')
+        Write-Output "The following domains were added to the Firewell Technology Solutions 'Safe Senders' list at '$tenantDomain':"
+        Write-Output $($newSafeDomains -join ', ')
+    }
 }
